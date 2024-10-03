c196c67b7d7ad8c1f0c551113cdf0cb015bf750d
Anh Duong Duc <anhdd6@officevnpay.com>
2024-10-03 15:40:26 +0700

    update

--- src/module/shared/shared.module.ts
+++ src/module/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { VaultModule } from './vault/vault.module';
 
 @Module({
-  imports: [VaultModule],
+  imports: [],
 })
 export class ShareModule {}
