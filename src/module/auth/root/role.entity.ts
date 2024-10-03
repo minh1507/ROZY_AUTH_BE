f0a11c0af05b5136076fcd5f1fa1c10ce3f1e705
Anh Duong Duc <anhdd6@officevnpay.com>
2024-10-03 17:28:45 +0700

    fix

--- src/module/auth/root/role.entity.ts
+++ src/module/auth/root/role.entity.ts
@@ -1,6 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MaxLength } from 'class-validator';
-import { Column, Entity, OneToMany } from 'typeorm';
+import { Entity, OneToMany } from 'typeorm';
 import { BaseEntity } from '../../../common/base/entity.base';
 import { User } from './user.entity';
 
