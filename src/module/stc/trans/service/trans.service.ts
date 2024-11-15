f7e8dbabd8977c842508cfcdd3dfdf6ee58e4f97
Duong Duc Anh <duongdoican@gmai.com>
2024-11-15 20:17:43 +0700

    update

---
+++ src/module/stc/trans/service/trans.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import { LoggerService } from "src/module/share/logger/logger.service";
+import SysHelper from "src/util/sys.util";
+
+@Injectable()
+export class TransService{
+    constructor(
+        private readonly logger: LoggerService
+    ){}
+
+    public get = (lang: string) : object => {
+        const language = SysHelper.getLang(lang)
+
+        const languageObj = Object.fromEntries(Object.entries(language));
+
+        this.logger.trace(`Get lang: ${lang} success`)
+
+        return languageObj;
+    }
+}
