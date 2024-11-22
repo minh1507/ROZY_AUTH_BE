1d680dfa7d8415169a5c4e21f0f05b921092aa81
Duong Duc Anh <duongdoican@gmai.com>
2024-11-22 20:10:04 +0700

    update

--- src/module/v1/stc/category/ethnic/entity/ethnic.entity.ts
+++ src/module/v1/stc/category/ethnic/entity/ethnic.entity.ts
@@ -2,4 +2,4 @@ import { Entity} from 'typeorm';
 import { BaseEntity } from 'src/common/base/entity.base';
 
 @Entity()
-export class Product extends BaseEntity {}
+export class Ethnic extends BaseEntity {}
